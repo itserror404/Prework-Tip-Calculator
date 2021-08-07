082fe2e10bf4c7599b6693a593913dfb4001d541
Maimuna Zaheer <mz2934@nyu.edu>
2021-08-07 17:20:23 +0530

    Initial Commit

---
+++ prework_tipcalc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prework_tipcalc
+//
+//  Created by Maimuna Zaheer on 07/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
